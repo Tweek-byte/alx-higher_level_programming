21361a80ccf62f6b6a83152e60afc72adfa3582f
Tweek <zaka4396@gmail.com>
2023-11-28 13:39:46 -0800

    task 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if 'a' <= c <= 'z':
+            cap = chr(ord(c) - 32)
+        else:
+            cap = c
+            
+        print("{:s}".format(cap), end="")
+    print("\n", end="")
