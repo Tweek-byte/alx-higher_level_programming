83185aeaf63660ef811a25860d160bfd2ffc77f7
Tweek <zaka4396@gmail.com>
2023-11-30 15:52:20 -0800

    task 5 fix & adv task 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,11 @@ if __name__ == "__main__":
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a, op, b = map(int, sys.argv[1:4])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
     operator = {"+": add, "-": sub, "*": mul, "/": div}
-    if op not in operator:
+    if op not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print(f"{a} {op} {b} = {operator[op](a, b)}")
+    print(f"{a} {op} {b} =", operator[op](a, b))
