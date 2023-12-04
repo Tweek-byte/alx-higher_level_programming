641a9ec74489ed4ab61a4fca9a5b4637f956e29e
Tweek <zaka4396@gmail.com>
2023-12-04 09:27:39 -0800

    task 7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index, value in enumerate(row):
+            if index != len(row) - 1:
+                print("{:d}".format(value), end=" ")
+            else:
+                print("{:d}".format(value))
