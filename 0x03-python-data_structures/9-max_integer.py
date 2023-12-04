1c8d7d47d7585d2cdc68e9307dc699f127920318
Tweek <zaka4396@gmail.com>
2023-12-04 11:55:15 -0800

    task 10

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = my_list[:]
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0:
-            new_list[i] = True
-        else:
-            new_list[i] = False
-    return new_list
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max = my_list[0]
+        for i in my_list:
+            if i > max:
+                max = i
+    return max
