805bed3b82565b849f5a5093cfc2b3b31fdc08d8
Tweek <zaka4396@gmail.com>
2023-12-04 09:15:54 -0800

    task 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    output = ""
     for s in my_string:
         if s != "C" and s != "c":
-            print("{:s}".format(s), end="")
+            output += s
+    return output
