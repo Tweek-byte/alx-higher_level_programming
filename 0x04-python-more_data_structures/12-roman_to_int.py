2b0db54482af1053075e35db39612d0e97f72928
Tweek <zaka4396@gmail.com>
2023-12-06 14:33:15 -0800

    task 11 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,18 +4,18 @@ def roman_to_int(roman_string):
 
     r_dic = {"I": 1, "V": 5, "X": 10,
              "L": 50, "C": 100, "D": 500, "M": 1000}
+
     res = 0
     prev_v = 0
-    for char in reversed(roman_string):
-        val = r_dic[char]
 
-        if val is None:
-            return 0
+    for char in reversed(roman_string):
+        val = r_dic.get(char, 0)
 
         if val < prev_v:
             res -= val
         else:
             res += val
+
         prev_v = val
 
     return res
