8bfa60b955d0fe5582445ded814550d05f3a6fcf
Tweek <zaka4396@gmail.com>
2023-12-06 13:44:52 -0800

    task 2 fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+def replace_element(element):
+return (element if element!= search else replace)
+return list(map(replace_element, my_list))
