4b13c6c4990d03e79ae192b4a69c2a336b0b77d3
Tweek <zaka4396@gmail.com>
2023-12-06 14:17:15 -0800

    task 11 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionary):
-        max = list(a_dictionary.keys())[0]
-    for key in a_dictionary:
-        if a_dictionary[key] > a_dictionary[max]:
-            max = key
-            return max
+    if not a_dictionary:
         return None
+    k = a_dictionary.get
+    return max(a_dictionary, k)
