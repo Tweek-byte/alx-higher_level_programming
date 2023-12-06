430b4f2eddd562c8249ac52a0a3460e31e36b065
Tweek <zaka4396@gmail.com>
2023-12-06 14:11:14 -0800

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    r_dictionary = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    di = [r_dictionary[c] for c in roman_string]
+    res = 0
+    for key, value in enumerate(di):
+        res += value
+        if key != 0 and di[key - 1] < value:
+            res -= (di[key - 1] + di[key - 1])
+    return res
