d14675195df0a8e98fe36d8e8eaa53615010b2aa
Tweek <zaka4396@gmail.com>
2023-12-18 14:18:51 -0800

    adv 1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     try:
-        print(" {:d}".format(value))
+        print("{:d}".format(value))
 
     except (ValueError, TypeError) as err:
         import sys
