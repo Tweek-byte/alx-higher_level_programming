5bc51858b86b23220344dc1fe174b8f9ef554f2a
Tweek <zaka4396@gmail.com>
2024-01-04 15:36:59 -0800

    adv

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,44 +1,65 @@
+#!/usr/bin/python3
+"""
+N-Queens backtracking program to print the coordinates of N queens
+on an NxN grid such that they are all in non-attacking positions.
+"""
+
 from sys import argv
 
-def initialize_chessboard(size):
-    return [[i, None] for i in range(size)]
-
-def already_exists(chessboard, y):
-    return any(y == queen[1] for queen in chessboard)
-
-def reject_solution(chessboard, x, y):
-    if already_exists(chessboard, y):
-        return False
-    return all(abs(queen[1] - y) != abs(i - x) for i, queen in enumerate(chessboard[:x]))
-
-def clear_chessboard(chessboard, start_index):
-    for i in range(start_index, len(chessboard)):
-        chessboard[i][1] = None
-
-def nqueens(chessboard, x):
-    size = len(chessboard)
-    for y in range(size):
-        clear_chessboard(chessboard, x)
-        if reject_solution(chessboard, x, y):
-            chessboard[x][1] = y
-            if x == size - 1:
-                print([(queen[0], queen[1]) for queen in chessboard])
-            else:
-                nqueens(chessboard, x + 1)
-
 if __name__ == "__main__":
+    chessboard = []
+    
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-
+    
     if not argv[1].isdigit():
         print("N must be a number")
         exit(1)
-
-    size = int(argv[1])
-    if size < 4:
+    
+    board_size = int(argv[1])
+    
+    if board_size < 4:
         print("N must be at least 4")
         exit(1)
 
-    chessboard = initialize_chessboard(size)
-    nqueens(chessboard, 0)
+    # Initialize the chessboard
+    for i in range(board_size):
+        chessboard.append([i, None])
+
+    def queen_already_exists(y):
+        """Check that a queen does not already exist in that y value."""
+        for x in range(board_size):
+            if y == chessboard[x][1]:
+                return True
+        return False
+
+    def reject_solution(x, y):
+        """Determines whether or not to reject the solution."""
+        if queen_already_exists(y):
+            return False
+        i = 0
+        while i < x:
+            if abs(chessboard[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_chessboard(x):
+        """Clears the chessboard from the point of failure on."""
+        for i in range(x, board_size):
+            chessboard[i][1] = None
+
+    def solve_nqueens(x):
+        """Recursive backtracking function to find the solution."""
+        for y in range(board_size):
+            clear_chessboard(x)
+            if reject_solution(x, y):
+                chessboard[x][1] = y
+                if x == board_size - 1:
+                    print(chessboard)
+                else:
+                    solve_nqueens(x + 1)  # Move on to the next x value to continue
+
+    # Start the recursive process at x = 0
+    solve_nqueens(0)
