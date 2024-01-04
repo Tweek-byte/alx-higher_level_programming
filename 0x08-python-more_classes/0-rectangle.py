9c33ddb6e74d01d00d1a0fc8093064d4a77ad8d0
Tweek <zaka4396@gmail.com>
2024-01-04 15:10:34 -0800

    task 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines an empty class named Rectangle
+"""
+
+
+class Rectangle:
+    """Empty representation of a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Soltuions for more python classes
