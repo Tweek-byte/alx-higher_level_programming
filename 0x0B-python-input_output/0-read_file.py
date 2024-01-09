2ff6e685c176b6eb6ee29ec8167f0dfe985c2640
Tweek <zaka4396@gmail.com>
2024-01-09 15:57:03 -0800

    task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ Contains the read_file function
 def read_file(filename=""):
     """This function reads a text file (UTF8) and prints it to stdout:"""
 
-    with open(filename, "r", encoding="utf-8") as FD:
-        print(FD.read(), end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
