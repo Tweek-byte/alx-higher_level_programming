5a0f0eb2064bbf7929aaac441c65cc4cc5123beb
Tweek <zaka4396@gmail.com>
2024-01-09 16:08:05 -0800

    task 3 fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,6 @@ Contains the append_write function
 
 
 def append_write(filename="", text=""):
-    """Appends the given text to a file (UTF8) and returns the number of characters added."""
+    """Appends the given text to a file (UTF8)"""
     with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
