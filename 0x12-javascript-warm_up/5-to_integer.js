a4d7988c47a6e2972c8241a5cdaf01d761efc2e3
Tweek-Byte <zaka4396@gmail.com>
2024-04-08 19:43:19 -0700

    task 2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
-#!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+!/usr/bin/node
+
+const n = Math.floor(process.argv[2]);
+if (Number.isInteger(n)) {
+  console.log(`My number: ${n}`);
+} else {
+  console.log('Not a number');
+}
