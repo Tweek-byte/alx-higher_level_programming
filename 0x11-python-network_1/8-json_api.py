d15b71b7cd58c04f7f752d6b66189d680316a64f
Tweek-Byte <zaka4396@gmail.com>
2024-04-26 13:19:43 -0700

    test 6

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Sends a POST request to http://0.0.0.0:5000/search_user """
 
-
 import sys
 import requests
 
-
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        harf = " "
-    else sys.argv[1]:
-        pd = {"q": harf}
+        harf = ""
+    else:
+        harf = sys.argv[1]
+
+    pd = {"q": harf}
 
     req = requests.post("http://0.0.0.0:5000/search_user", pd)
     try:
