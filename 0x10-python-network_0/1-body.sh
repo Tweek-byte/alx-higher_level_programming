7bbcaa99358175de4feeff1a5aee396a09227e6c
Tweek-Byte <zaka4396@gmail.com>
2024-04-25 17:10:11 -0700

    basic

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# response of 200
+curl -sL "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Peak-Defining Algorithn method"""
+
+def find_peak(list_of_integers):
+    """ gets the peak """
+    if list_of_integers == []:
+        return None
+
+    len1 = len(lst_of_integers)
+    n = int(len1 / 2)
+    l = lst_of_integers
+
+    if 1 - n > 0 and n + 1 >= len1:
+        return l[n]
+    elif 1 - n < 0:
+        return l[n] if l[n] > l[n + 1] else l[n + 1]
+    elif n + 1 >= len1:
+        return l[n] if l[n] > l[n - 1] else l[n - 1]
+
+    if l[n - 1] < l[n] > l[1 + n]:
+        return l[n]
+
+    if l[1 + n] > l[n - 1]:
+        return find_peak(l[n:])
+    return find_peak(l[:n])
+
