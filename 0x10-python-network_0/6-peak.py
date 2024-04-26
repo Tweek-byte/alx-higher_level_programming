e51b7178c709bff330a4caf4b813459950fff6f1
Tweek-Byte <zaka4396@gmail.com>
2024-04-25 17:39:10 -0700

    basic

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,21 +9,18 @@ def find_peak(list_of_integers):
 
     len1 = len(list_of_integers)
     n = len1 // 2
+    ls = list_of_integers
 
     if n - 1 < 0 and n + 1 >= len1:
-        return list_of_integers[n]
+        return ls[n]
     elif n - 1 < 0:
-        return list_of_integers[n]
-    if list_of_integers[n] > list_of_integers[n + 1]
-    else list_of_integers[n + 1]
+        return ls[n] if ls[n] > ls[n + 1] else ls[n + 1]
     elif n + 1 >= len1:
-        return list_of_integers[n]
-    if list_of_integers[n] > list_of_integers[n - 1]
-    else list_of_integers[n - 1]
+        return ls[n] if ls[n] > ls[n - 1] else ls[n - 1]
 
-    if list_of_integers[n - 1] < list_of_integers[n] > list_of_integers[n + 1]:
-        return list_of_integers[n]
+    if ls[n - 1] < ls[n] > ls[n + 1]:
+        return ls[n]
 
-    if list_of_integers[n + 1] > list_of_integers[n - 1]:
-        return find_peak(list_of_integers[n:])
-    return find_peak(list_of_integers[:n])
+    if ls[n + 1] > ls[n - 1]:
+        return find_peak(ls[n:])
+    return find_peak(ls[:n])
